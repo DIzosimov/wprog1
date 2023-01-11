c9eba5efbdea4cc77b12bb07c5c7735f0576cdd4
DIzosimov <david.izosimov@gmail.com>
2023-01-11 16:08:26 +0100

    Adds dropdown menu

--- gesäll/scripts/home.js
+++ gesäll/scripts/home.js
@@ -91,11 +91,14 @@ window.addEventListener("load", () => {
 const windowResize = () => {
   let width = window.innerWidth;
   let nav = document.getElementById('navContainer')
+  let dropdown = document.getElementById('drop')
   if (width < 768) {
     nav.style.display = "none"
+    //dropdown.style.display = "block"
   } 
   if (nav.style.display !== "flex" && width >= 768) {
     document.getElementById('navContainer').style.display = "flex"
+    //document.getElementById('dropdown').style.display = "none"
   }
 }
 
